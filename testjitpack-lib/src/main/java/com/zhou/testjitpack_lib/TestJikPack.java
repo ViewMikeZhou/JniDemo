55f5548e9d7e59cca7fb9733139b942594e935ae
PC-201702211611\Administrator <mrzhoul@sina.com>
2017-11-21 14:08:20 +0800

    添加jitPack库

---
+++ testjitpack-lib/src/main/java/com/zhou/testjitpack_lib/TestJikPack.java
@@ -0,0 +1,13 @@
+package com.zhou.testjitpack_lib;
+
+import android.util.Log;
+
+/**
+ * Created by zhou on 2017/11/21.
+ */
+
+public class TestJikPack {
+    public static  void test(){
+        Log.e("test","test ---->");
+    }
+}
